60f3d8cee967700346d409e06504e2d13eb55990
Akash Sare <akashsare03@gmail.com>
2025-07-25 16:32:27 +0530

    Added Flask in the requirements.txt file and updated flask.app.py file

--- flask_app.py
+++ flask_app.py
@@ -57,6 +57,9 @@ def callback():
 
     return redirect("https://smart-expense-tracker-sldw.onrender.com")  # 🔁 Your Streamlit app link here
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+#if __name__ == "__main__":
+ #   app.run(host="0.0.0.0", port=10000)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
